555bf9475e155617b7994047908bf05ba8135cbb
acid511 <241877609+acid511@users.noreply.github.com>
2026-02-06 17:16:12 +0900

    バージョン変更

--- thprac/src/thprac/thprac_version.h
+++ thprac/src/thprac/thprac_version.h
@@ -2,11 +2,11 @@
 
 namespace THPrac {
 
-	
-#define THPRAC_VERSION_0 2
-#define THPRAC_VERSION_1 2
-#define THPRAC_VERSION_2 2
-#define THPRAC_VERSION_3 2
+
+#define THPRAC_VERSION_0 0
+#define THPRAC_VERSION_1 9
+#define THPRAC_VERSION_2 0
+#define THPRAC_VERSION_3 0
 
 #define STRR(X) #X
 #define STR(X) STRR(X)
