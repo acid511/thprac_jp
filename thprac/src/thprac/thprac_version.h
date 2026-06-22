a4406f5b2d36c973bc29bf1b0e6127d984a74d0b
acid511 <241877609+acid511@users.noreply.github.com>
2026-06-23 00:47:02 +0900

    バージョン更新

--- thprac/src/thprac/thprac_version.h
+++ thprac/src/thprac/thprac_version.h
@@ -30,10 +30,10 @@ struct ThpracVersion {
 };
 
 // literal macros needed for thprac.rc
-#define THPRAC_VERSION_0 2
-#define THPRAC_VERSION_1 2
-#define THPRAC_VERSION_2 2
-#define THPRAC_VERSION_3 7
+#define THPRAC_VERSION_0 1
+#define THPRAC_VERSION_1 0
+#define THPRAC_VERSION_2 4
+#define THPRAC_VERSION_3 0
 inline constexpr ThpracVersion currentVersion = { THPRAC_VERSION_0, THPRAC_VERSION_1, THPRAC_VERSION_2, THPRAC_VERSION_3 };
 
 #define STRR(X) #X
